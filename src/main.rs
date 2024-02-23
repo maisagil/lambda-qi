7dce3f2d988b4f31540b9bfcc804bf0d010fc4b3
Eduardo Loback <eduardolloback@gmail.com>
2024-02-23 03:00:52 -0300

    chore: removed lambda and tower code.

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,14 @@ pub mod configuration;
 mod qitech;
 
 use configuration::get_configuration;
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use qitech::{AskBalanceRequest, QiTechProvider};
 use std::sync::OnceLock;
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 static INSTANCE: OnceLock<QiTechProvider> = OnceLock::new();
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, anyhow::Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-    let request = AskBalanceRequest {
-        document_number: "05577889944".to_string(),
-    };
-    let provider = INSTANCE.get().unwrap();
-    match provider.ask_for_balance(request).await {
-        Ok(response) => println!("{:#?}", &response),
-        Err(e) => println!("{:#?}", e),
-    }
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
-
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), anyhow::Error> {
     dotenv::dotenv().ok();
 
     let configuration = get_configuration().expect("Failed to read configuration.");
@@ -58,5 +27,13 @@ async fn main() -> Result<(), Error> {
         .without_time()
         .init();
 
-    run(service_fn(function_handler)).await
+    let request = AskBalanceRequest {
+        document_number: "05577889944".to_string(),
+    };
+    let provider = INSTANCE.get().unwrap();
+    match provider.ask_for_balance(request).await {
+        Ok(response) => println!("{:#?}", &response),
+        Err(e) => println!("{:#?}", e),
+    };
+    Ok(())
 }
